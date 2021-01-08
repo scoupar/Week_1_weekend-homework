3f08341c158151bb0c60d5cef35998dfc4745987
Scott Coupar <scottcoupar@Scotts-MacBook-Pro.local>
2021-01-08 15:56:44 +0000

    made some changes

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,14 +1,22 @@
 # WRITE YOUR FUNCTIONS HERE
 
-def get_pet_shop_name(name):
-    return "Camelot of Pets"
+def get_pet_shop_name(list):
+    for name in list:
+        return "Camelot of Pets"
+    get_pet_shop_name(cc_pet_shop)
 
 
 
-def get_total_cash(total_cash):
-    return 1000
+
+# def get_total_cash(total_cash):
+#     return 1000
 
 
+# def add_or_remove_cash(total_cash, added_cash):
+#     added_cash = 10
+#     return 
+
+    
 
 
 
